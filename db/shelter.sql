ed8c7afed097ae72f013e9daa0451fe43c32c07c
Graeme Henderson <graeme.189@gmail.com>
2019-07-19 12:42:48 +0100

    Create tables

--- db/shelter.sql
+++ db/shelter.sql
@@ -1,10 +1,10 @@
-DROP TABLE animals
-DROP TABLE owners
+DROP TABLE animals;
+DROP TABLE owners;
 
 CREATE TABLE owners(
   id serial4 PRIMARY KEY,
   name VARCHAR(255)
-)
+);
 
 CREATE TABLE animals(
   id serial4 PRIMARY KEY,
@@ -12,5 +12,5 @@ CREATE TABLE animals(
   type VARCHAR(255),
   breed VARCHAR(255),
   admission_date INT4,
-  owner_id REFERENCES owners(id) ON DELETE CASCADE
-)
+  owner_id int4 REFERENCES owners(id) ON DELETE CASCADE
+);
