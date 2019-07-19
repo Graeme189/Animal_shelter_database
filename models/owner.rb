a9f454d55ea67961d286dd029622f7161b564e6e
Graeme Henderson <graeme.189@gmail.com>
2019-07-19 16:13:02 +0100

    Add find by id method to Owner class

--- models/owner.rb
+++ models/owner.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner')
 
 class Owner
 
-attr_reader :id, :first_name, :last_name
+  attr_reader :id, :first_name, :last_name
 
   def initialize(options)
     @id = options['id'].to_i
@@ -38,13 +38,22 @@ attr_reader :id, :first_name, :last_name
     return result
   end
 
-def animals()
-  sql = "SELECT * FROM animals
-  WHERE owner_id = $1"
-  values = [@id]
-  results = SqlRunner.run(sql, values)
-  animals = results.map { |animal| Animal.new(animal)}
-  return animals
-end
+  def self.find(id)
+    sql = "SELECT * FROM owners
+    WHERE id = $1"
+    values = [id]
+    owner = SqlRunner.run(sql, values)
+    result = Owner.new(owner.first)
+    return result
+  end
+
+  def animals()
+    sql = "SELECT * FROM animals
+    WHERE owner_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    animals = results.map { |animal| Animal.new(animal)}
+    return animals
+  end
 
 end
