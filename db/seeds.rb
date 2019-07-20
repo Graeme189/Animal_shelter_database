b53c6b03df5377ab9939c5c3e7de87f0bfa8cd61
Graeme Henderson <graeme.189@gmail.com>
2019-07-20 14:49:15 +0100

    Add more entries to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,33 @@ owner3 = Owner.new({
   "last_name" => "Bramley"
   })
 
+owner4 = Owner.new({
+  "first_name" => "Kevin",
+  "last_name" => "Field"
+  })
+
+owner5 = Owner.new({
+  "first_name" => "Eleanor",
+  "last_name" => "Evans"
+  })
+
+owner6 = Owner.new({
+  "first_name" => "Luise",
+  "last_name" => "Valentiner"
+  })
+
+owner7 = Owner.new({
+  "first_name" => "Gilles",
+  "last_name" => "Moulin"
+  })
+
 owner1.save()
 owner2.save()
 owner3.save()
+owner4.save()
+owner5.save()
+owner6.save()
+owner7.save()
 
 animal1 = Animal.new({
   "name" => "Lola",
@@ -52,9 +76,55 @@ animal3 = Animal.new({
   "adoption_ready" => true
   })
 
+animal4 = Animal.new({
+  "name" => "Bowie",
+  "type" => "dog",
+  "breed" => "collie",
+  "admission_date" => "15-01-2019",
+  "adoption_ready" => true
+  })
+
+animal5 = Animal.new({
+  "name" => "Obama",
+  "type" => "dog",
+  "breed" => "daschund",
+  "admission_date" => "14-02-2012",
+  "owner_id" => owner6.id,
+  "adoption_ready" => false
+  })
+
+animal6 = Animal.new({
+  "name" => "Long John Silver",
+  "type" => "parrot",
+  "breed" => "macaw",
+  "admission_date" => "19-07-2019",
+  "adoption_ready" => false
+  })
+
+animal7 = Animal.new({
+  "name" => "Incy Wincy Spider",
+  "type" => "spider",
+  "breed" => "tarantula",
+  "admission_date" => "12-11-2018",
+  "adoption_ready" => true
+  })
+
+animal8 = Animal.new({
+  "name" => "Henry",
+  "type" => "hippo",
+  "breed" => "pigmy",
+  "admission_date" => "04-05-2019",
+  "adoption_ready" => false
+  })
+
   animal1.save()
   animal2.save()
   animal3.save()
+  animal4.save()
+  animal5.save()
+  animal6.save()
+  animal7.save()
+  animal8.save()
 
   binding.pry
   nil
