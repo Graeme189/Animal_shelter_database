d98e013f331de9c2843692c763b595816ca032fe
Graeme Henderson <graeme.189@gmail.com>
2019-07-20 13:54:05 +0100

    Create test spec file for Animal class

---
+++ models/specs/animal_specs.rb
@@ -0,0 +1,12 @@
+require("minitest/autorun")
+require_relative("../animal")
+
+class TestAnimal < MiniTest::Test
+
+  def setup
+    options = {"id" => 1, "name" => "Susie","type" => "cat", "breed" => "moggie", "admission_date" => "12th June 2018", "owner_id" => "1", "adoption_ready" => true}
+
+    @animal1 = Animal.new(options)
+  end
+
+end
